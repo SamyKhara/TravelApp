34349abc218a5f9c23c0270f76980021afa0d356
SamyKhara <simransinghkhara@gmail.com>
2016-10-30 05:04:51 +0530

    The points come up and line is displayed. The line is hardcoded at this point to 3 points only.

--- app/src/main/java/com/example/samy/travelapp/MainActivity.java
+++ app/src/main/java/com/example/samy/travelapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.samy.travelapp;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -31,8 +34,11 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,
@@ -40,6 +46,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     GoogleMap mGoogleMap;
     GoogleApiClient mGoogleApiClient;
+    Marker marker;
+    Polyline line;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,11 +84,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
+        System.out.println("\n\n**** In onMapReady function ****");
         mGoogleMap = googleMap;
 
-        // goToLocationZoom(28.576990, 77.043202, 15);
+        //goToLocationZoom(28.576990, 77.043202, 13);
 
-       if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            System.out.println("#### In checkSelfPermission");
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
@@ -90,12 +101,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        //This over hear gives us that 'MyLocation' Button in the Maps which is used to get to my present location!
+        System.out.println("\n\n**** Before setMyLocationEnabled function ****");
         mGoogleMap.setMyLocationEnabled(true);
-
-        System.out.println("\n\n****Working Till Here!****");
-
-       /*
+        //This over hear gives us that 'MyLocation' Button in the Maps which is used to get to my present location!
+        System.out.println("\n\n**** After setMyLocationEnabled function ****");
+    }
+        /*
        //This code gets us to our present location the moment map starts!
        mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(LocationServices.API)
@@ -106,69 +117,136 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         //Object created but never connected to the Google Api Client
         mGoogleApiClient.connect();*/
 
-    }
-
     private void goToLocationZoom(double lat, double lng, float zoom) {
         LatLng ll = new LatLng(lat, lng);
         CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, zoom);
         mGoogleMap.moveCamera(update);
     }
 
-    Marker marker;
+
 
     public void geoLocate(View view) throws IOException {
 
-        EditText et = (EditText) findViewById(R.id.main_input1);
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("Dwarka Sector 21");
+        arr.add("Dwarka Sector 10");
+        arr.add("Dwarka Sector 12");
 
-        String location = et.getText().toString();
-
-        if(location==null){
-            Toast.makeText(this, "No location entered!", Toast.LENGTH_SHORT).show();
+        //Finding my  present location and getting there!
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
         }
+        Location myLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-        Geocoder gc = new Geocoder(this);
+        double latitude = myLocation.getLatitude();
+        double longitude = myLocation.getLongitude();
+        System.out.println("## myLocation latitude: "+ latitude);
+        System.out.println("## myLocation longitude: "+ longitude);
 
-        List<android.location.Address> list = gc.getFromLocationName(location, 1);
-        android.location.Address address = list.get(0);
-        String locality = address.getLocality();
+        //If you are testing this on your phone in singapore, use the following present location:
+        //latitude= 28.57713316;
+        //longitude= 77.04353435;
 
-        Toast.makeText(this, locality, Toast.LENGTH_SHORT).show();
+        //Working on setting marker points
+        String location;
+        int i;
 
-        double lat = address.getLatitude();
-        double lng = address.getLongitude();
-        goToLocationZoom(lat, lng, 15);
+        for (i=0;i<3;i++)
+        {
+            if (i==0){
+                //Home means Present Location of the person!
+                setMarker("Home", latitude, longitude, -1);
+            }
 
-        //This is how you set up Marker for positions!
+            location= arr.get(i);
+            System.out.println("$$$$ Location "+i+" : "+location);
 
-        setMarker(locality, lat, lng);
+            Geocoder gc = new Geocoder(this);
+
+            List<android.location.Address> list = gc.getFromLocationName(location, 1);
+
+            android.location.Address address = list.get(0);
+
+            String locality = address.getLocality();
+
+            double lat = address.getLatitude();
+            double lng = address.getLongitude();
+
+            setMarker(location, lat, lng, i);
+        }
+        System.out.println("******Loop has finished!");
+
+        //Takes me straight to my present location since line starts from there!
+        goToLocationZoom(latitude,longitude,13);
     }
 
-    //This method is only for Setting marker and checking if any prev exist and if so, remove it!
-    private void setMarker(String locality, double lat, double lng) {
 
-        if (marker!=null){
-            removePrevinstances();
-        }
+    Marker marker1;
+    Marker marker2;
+    Marker marker3;
+    Marker marker4;
 
+    //This method is only for Setting marker and then drawing the line. Is hardcoded as of now!
+    private void setMarker(String location, double lat, double lng, int i) {
+
+        i=i+1;
+        //        if (marker!=null){
+//            removePrevinstances();
+//        }
+
+        System.out.println("\n\nI am in setMarker function");
         MarkerOptions options= new MarkerOptions()
-                            .title(locality)
-                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+                            .title(location)
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
                             .position(new LatLng(lat,lng))
-                            .snippet("I am here");
+                            .snippet("Point #: "+i);
 
-        
+        if(marker1==null){
+            marker1= mGoogleMap.addMarker(options);
+        }else if (marker2==null){
+            marker2= mGoogleMap.addMarker(options);
+        }else if (marker3==null){
+            marker3= mGoogleMap.addMarker(options);
+        }else  if (marker4==null){
+            marker4= mGoogleMap.addMarker(options);
+            drawLine();
+        }else{
+            System.out.println("!!!!!!! IT HAS COME INTO ELSE !!!!!!");
+        }
 
-        marker= mGoogleMap.addMarker(options);
     }
 
 
-    private void removePrevinstances(){
-        marker.remove();
-        marker= null;
+    private void drawLine(){
+        PolylineOptions options = new PolylineOptions();
+
+
+            options.add(marker1.getPosition())
+                    .add(marker2.getPosition())
+                    .add(marker3.getPosition())
+                    .add(marker4.getPosition())
+                    .color(Color.RED)
+                    .width(4);
+
+        line=mGoogleMap.addPolyline(options);
     }
+
+//Code to remove markers
+//    private void removePrevinstances(){
+//        marker.remove();
+//        marker= null;
+//    }
+
 
     LocationRequest mLocationRequest;
-
     @Override
     public void onConnected(@Nullable Bundle bundle) {
         mLocationRequest = LocationRequest.create();
